bb617edbe0b141e147e8a5f121671433c8928928
Paramvir Deol <paramvir.deol@gmail.com>
2015-12-27 20:55:39 +0530

    Adding a basic calculator

---
+++ SimpleCalculator.cs
@@ -0,0 +1,29 @@
+using System;
+
+
+public class SimpleCalculator
+{
+	public double Add(double first, double second)
+	{
+		// Add two numbers
+		return first + second;
+	}
+	
+	public double Subtract(double first, double second)
+	{
+		// do something
+		return first - second;
+	}
+	
+	public double Multiply(double first, double second)
+	{
+		// do something
+		return first * second;
+	}
+	
+	public double Divide(double first, double second)
+	{
+		// do something
+		return first / second;
+	}
+} 
